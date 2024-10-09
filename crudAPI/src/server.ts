45e53b26b29d95cdf6d8c5b487f0ddce724a07fa
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-10-09 11:06:56 -0300

    relation between user and contact done

--- crudAPI/src/server.ts
+++ crudAPI/src/server.ts
@@ -1,3 +1,17 @@
-import fastify from "fastify";
+import fastify, { FastifyInstance } from "fastify";
 
-const app = fastify();
+const app: FastifyInstance = fastify({logger: true});
+
+app.get('/', async (request, reply) => {
+    return { hello: 'world' }
+})
+  
+const start = async () => {
+    try {
+      await app.listen({port: 3000})
+    } catch (err) {
+      app.log.error(err)
+      process.exit(1)
+    }
+}
+start()
